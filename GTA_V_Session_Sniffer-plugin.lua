46803e8f0a2413e6e269125faa1aef40ad45a3c5
IB_U_Z_Z_A_R_Dl <BUZZARDGTA@protonmail.com>
2024-11-29 18:00:34 +0100

    Update GTA_V_Session_Sniffer-plugin.lua

--- GTA_V_Session_Sniffer-plugin.lua
+++ GTA_V_Session_Sniffer-plugin.lua
@@ -3,6 +3,13 @@
 -- Allows you to automatically have every usernames showing up on GTA V Session Sniffer project, by logging all players from your sessions to "Cherax\Lua\GTA_V_Session_Sniffer-plugin\log.txt".
 -- GitHub Repository: https://github.com/Illegal-Services/GTA_V_Session_Sniffer-plugin-Cherax-Lua
 
+-- DISCLAIMER:
+-- Cherax API is trash, I've no idea how to create these things because Lua repos are closed-source so impossible for me to code them:
+-- create_tick_handler() OR threads where we can use yield() inside.
+----- I've created a dummy toogle option in "Lua Content" tab literally only for that purpose, it makes literally no sense but wathever.
+-- event listeners (eCallbackTrigger.OnPlayerLeave) how tf am I supposed to use that?? no examples. I get how to assign it to a Feature but how about literally any thread/function? lol
+----- I've removed a part of the code using that cuz I simply cannot magically find out how to code it.
+
 -- Globals START
 ---- Global variables START
 local player_join__timestamps = {}
@@ -12,6 +19,14 @@ local player_join__timestamps = {}
 local SCRIPT_NAME <const> = "GTA_V_Session_Sniffer-plugin.lua"
 local SCRIPT_TITLE <const> = "GTA V Session Sniffer"
 local SCRIPT_LOG__PATH <const> = FileMgr.GetMenuRootPath() .. "\\Lua\\GTA_V_Session_Sniffer-plugin\\log.txt"
+local NATIVES <const> = {
+    --[[
+    Scrapped from: https://alloc8or.re/gta5/nativedb/
+
+    This is up-to-date for b3351
+    ]]
+    NETWORK_IS_SESSION_STARTED = 0x9DE624D2FC4B603F
+}
 ---- Global constants END
 
 ---- Global functions START
@@ -130,32 +145,35 @@ local function write_to_log_file(player_entries_to_log)
     log_file:close()
 end
 
+
 SessionSnifferLogging_Feat = FeatureMgr.AddFeature(Utils.Joaat("SessionSnifferLogging"), "Toggle Session Sniffer Logging", eFeatureType.Toggle, "Toggle Session Sniffer Logging", function()
-
     if FeatureMgr.IsFeatureToggled(Utils.Joaat("SessionSnifferLogging")) then
-        if Natives.InvokeBool(0x9DE624D2FC4B603F) then  -- if Natives.InvokeBool(Utils.Joaat("NETWORK_IS_SESSION_STARTED")) then
-            local player_entries_to_log = {}
+        if not FileMgr.DoesFileExist(SCRIPT_LOG__PATH) then
+            create_empty_file(SCRIPT_LOG__PATH)
+        end
 
-            local log__content, err = read_file(SCRIPT_LOG__PATH)
+        local log__content, err = read_file(SCRIPT_LOG__PATH)
+        if err then
+            handle_script_exit({ hasScriptCrashed = true })
+            return
+        end
+
+        if is_file_string_need_newline_ending(log__content) then
+            local file, err = io.open(SCRIPT_LOG__PATH, "a")
             if err then
                 handle_script_exit({ hasScriptCrashed = true })
                 return
             end
 
-            if is_file_string_need_newline_ending(log__content) then
-                local file, err = io.open(SCRIPT_LOG__PATH, "a")
-                if err then
-                    handle_script_exit({ hasScriptCrashed = true })
-                    return
-                end
+            file:write("\n")
+            file:close()
+        end
 
-                file:write("\n")
-                file:close()
-            end
-
+        if Natives.InvokeBool(NATIVES.NETWORK_IS_SESSION_STARTED) then
+            local player_entries_to_log = {}
             local currentTimestamp = Time.GetEpoche()
             for _, playerID in pairs(Players.Get()) do
-                -- Getting the player SCID in Cherax is cringe
+                -- Getting the player SCID and IP in Cherax is cringe
                 local CNetGamePlayer = Players.GetById(playerID)
                 if CNetGamePlayer then
                     local GamerInfo = CNetGamePlayer:GetGamerInfo()
@@ -163,10 +181,13 @@ SessionSnifferLogging_Feat = FeatureMgr.AddFeature(Utils.Joaat("SessionSnifferLo
                         if GamerInfo.RockstarId then
                             local playerSCID = GamerInfo.RockstarId
                             local playerName = Players.GetName(playerID)
-                            local playerIP = Players.GetIPString(playerID)
-                            local cleanedPlayerIP = playerIP:match("^(%d+%.%d+%.%d+%.%d+)") -- Cherax API is really cringe
+                            local playerIPString = Players.GetIPString(playerID)
+                            local playerIP
+                            if playerIPString then
+                                playerIP = playerIPString:match("^(%d+%.%d+%.%d+%.%d+) %(Direct%)") -- Cherax API is really cringe
+                            end
 
-                            loggerPreTask(player_entries_to_log, log__content, currentTimestamp, playerID, playerSCID, playerName, cleanedPlayerIP)
+                            loggerPreTask(player_entries_to_log, log__content, currentTimestamp, playerID, playerSCID, playerName, playerIP)
                         end
                     end
                 end
@@ -179,11 +200,8 @@ SessionSnifferLogging_Feat = FeatureMgr.AddFeature(Utils.Joaat("SessionSnifferLo
         else
             player_join__timestamps = {}
         end
-
     end
-
 end)
-
 
 ClickGUI.AddTab(SCRIPT_TITLE, function()
 	if ClickGUI.BeginCustomChildWindow("Session Sniffer Logging") then
@@ -193,30 +211,5 @@ ClickGUI.AddTab(SCRIPT_TITLE, function()
 end)
 
 
-local function handle_player_leave(playerID)
-    print("player left:")
-    print(playerID)
-    player_join__timestamps[playerID] = nil
-end
-
-eCallbackTrigger.OnPlayerLeave = handle_player_leave
---playerLeaveEventListener = eCallbackTrigger.OnPlayerLeave(function(f)
---    handle_player_leave(f)
---end)
-
 -- === Main Loop === --
-if not FileMgr.DoesFileExist(SCRIPT_LOG__PATH) then
-    create_empty_file(SCRIPT_LOG__PATH)
-end
-
 SessionSnifferLogging_Feat:RegisterCallbackTrigger(eCallbackTrigger.OnTick)
---SessionSnifferLogging_Feat:RegisterCallbackTrigger(eCallbackTrigger.OnTick)
----while true do
---if FeatureMgr.IsFeatureToggled(Utils.Joaat("SessionSnifferLogging")) then
-
-
---SetShouldUnload()
-
---
---end
----end
